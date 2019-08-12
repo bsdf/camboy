fa9f699a0b808334595831fa9270f5ffbe6ff7a6
BEN ENGLISCH <EMAILBEN145@gmail.com>
2019-08-12 13:35:51 -0500

    swap out clap for structopt

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,47 @@
-mod parse;
 mod image;
+mod parse;
 
-use std::{fs::File, io::Result};
-use clap::{App, Arg, ArgMatches};
+use std::{fs::File, io::Result, path::PathBuf};
+use structopt::StructOpt;
 
-const ARG_SAVE_FILE: &'static str = "save_file";
-const ARG_OUTPUT_FMT: &'static str = "output";
-const ARG_PICTURE_IDX: &'static str = "picture_index";
+#[derive(Debug, StructOpt)]
+#[structopt(name = "CAMBOY")]
+/// camboy's about string maybbe
+struct Opt {
+    #[structopt(name = "picture number", short = "i", long = "index")]
+    /// dump a single picture, indexed by 0.
+    picture_index: Option<usize>,
+
+    #[structopt(
+        name = "format",
+        short = "o",
+        long = "out",
+        default_value = "picture-%"
+    )]
+    /// filename output format. the '%' character will be replaced with the picture number.
+    output_format: String,
+
+    #[structopt(name = "INPUT", parse(from_os_str))]
+    /// the gameboy camera save file dump.
+    input_file: PathBuf,
+}
 
 fn main() -> Result<()> {
-    let args = parse_args();
-    let filename = args.value_of(ARG_SAVE_FILE).unwrap();
-    let out_fmt = args.value_of(ARG_OUTPUT_FMT).unwrap();
-    let picture_idx = args.value_of(ARG_PICTURE_IDX);
+    let opt = Opt::from_args();
+    let file = File::open(&opt.input_file).expect("could not open file.");
 
-    let file = File::open(filename).expect("could not open file.");
-
-    match picture_idx {
+    match opt.picture_index {
         Some(idx) => {
             // user provided a picture index
-            let idx: usize = usize::from_str_radix(idx, 10).unwrap();
-            image::save_photo(&file, idx, get_filename(&out_fmt, idx))
+            image::save_photo(&file, idx, get_filename(&opt.output_format, idx))
         }
         None => {
             // no picture index, dump all pictures
             (0..30)
-                .map(|i| image::save_photo(&file, i, get_filename(&out_fmt, i)))
+                .map(|i| image::save_photo(&file, i, get_filename(&opt.output_format, i)))
                 .collect()
         }
     }
-}
-
-fn parse_args<'a>() -> ArgMatches<'a> {
-    App::new("gbcam parser")
-        .version("1.0")
-        .author("me")
-        .about("parses gbc camera save file")
-        .arg(
-            Arg::with_name(ARG_SAVE_FILE)
-                .value_name("INPUT")
-                .help("the gameboy camera save file dump")
-                .required(true)
-                .index(1))
-        .arg(
-            Arg::with_name(ARG_PICTURE_IDX)
-                .short("i")
-                .long("index")
-                .value_name("picture #")
-                .help("dump a single picture, indexed by 0.")
-                .takes_value(true))
-        .arg(
-            Arg::with_name(ARG_OUTPUT_FMT)
-                .short("o")
-                .long("out")
-                .default_value("picture-%"))
-        .get_matches()
 }
 
 fn get_filename(format: &str, idx: usize) -> String {
